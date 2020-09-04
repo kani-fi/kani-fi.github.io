04cfa699365b932730ba17e7e85e16cffe2d988c
kanifi@88.com <kanifi@88.com>
2020-09-05 00:46:14 +0800

    init kani website

---
+++ static/js/14.5385774d.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpkani-finance"]=this["webpackJsonpkani-finance"]||[]).push([[14],{1691:function(n,i){}}]);
+//# sourceMappingURL=14.5385774d.chunk.js.map
---
+++ static/js/9.5c52715f.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpkani-finance"]=this["webpackJsonpkani-finance"]||[]).push([[9],{1783:function(n,i){},1791:function(n,i){}}]);
+//# sourceMappingURL=9.5c52715f.chunk.js.map
