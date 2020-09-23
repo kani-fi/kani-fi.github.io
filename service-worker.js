c8bb4d45caa87c8831d58a7b8d7540f4d580c94a
kanifi@88.com <kanifi@88.com>
2020-09-23 18:21:27 +0800

    connect bug fix

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.c3a0c56233d6b5e2d36f461ac3d9a057.js"
+  "./precache-manifest.50f0cd618899fb7ffdbf24a0bb013274.js"
 );
 
 self.addEventListener('message', (event) => {
